3373c04ea63470bd97f11f04268048977a503da9
Serena Chen <serena.jing.chen@gmail.com>
2018-10-31 23:10:55 +0800

    remove eslint-loader

--- front-end/nuxt.config.js
+++ front-end/nuxt.config.js
@@ -64,12 +64,7 @@ module.exports = {
     extend(config, ctx) {
       // Run ESLint on save
       if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
+       
       }
     }
   }
